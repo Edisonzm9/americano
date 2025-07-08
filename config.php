f1363b6c467438f845d781499f2dbca5b49c4df4
edisonzm9 <eazamoram99@gmail.com>
2025-07-08 02:18:56 -0500

    fist commit

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle_local';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+// Tell Moodle where to find the openssl.cnf file on XAMPP for LTI to work correctly.
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    putenv('OPENSSL_CONF=C:\\xampp\\apache\\conf\\openssl.cnf');
+}
+
+$CFG->wwwroot   = 'http://localhost/moodle';
+$CFG->dataroot  = 'C:\\xampp\\moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+// Forzamos el uso del tema 'boost' para recuperar el acceso
+$CFG->theme = 'boost';
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
